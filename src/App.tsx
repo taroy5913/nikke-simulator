6d56db29f8e155df64425d84639df0a9616bce61
taroy5913 <taroy5913@gmail.com>
2022-12-06 19:02:58 +0900

    #13 #6 最短、最長の表示

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,7 @@ const simulate = (props: Props): Sample => {
     if (t % 7 == 0) {
       gems += 300;
       highQualityMolds += 10;
+      vouchers += 1;
     }
 
     // event bonus
@@ -190,28 +191,56 @@ const simulate = (props: Props): Sample => {
   }
   return res;
 }
-const predict = (params: Props): Sample => {
-  let res: Sample = {
-    days: 0,
-    friendVouchers: 0,
-    spareBodies: 0,
-    advancedSpareBodies: 0,
-    vouchers: 0,
-    advancedVouchers: 0,
-    highQualityVouchers: 0,
-    middleQualityVouchers: 0
-  };
-  const num = 100;
+const getDefaultSample = (defaultValue:number): Sample => {
+  return {
+    days: defaultValue,
+    friendVouchers: defaultValue,
+    spareBodies: defaultValue,
+    advancedSpareBodies: defaultValue,
+    vouchers: defaultValue,
+    advancedVouchers: defaultValue,
+    highQualityVouchers: defaultValue,
+    middleQualityVouchers: defaultValue
+  }
+}
+const predict = (params: Props, num:number = 1000): {min:Sample, max:Sample, avg:Sample} => {
+  let res = {
+    min: getDefaultSample(num),
+    max: getDefaultSample(0),
+    avg: getDefaultSample(0)
+  }
+
   for (let k = 0; k < num; ++k) {
     const sample = simulate(params);
-    res.days += sample.days / num;
-    res.friendVouchers += sample.friendVouchers / num;
-    res.spareBodies += sample.spareBodies / num;
-    res.advancedSpareBodies += sample.advancedSpareBodies / num;
-    res.vouchers += sample.vouchers / num;
-    res.advancedVouchers += sample.advancedVouchers / num;
-    res.highQualityVouchers += sample.highQualityVouchers / num;
-    res.middleQualityVouchers += sample.middleQualityVouchers / num;
+    // 最短
+    res.min.days = Math.min(res.min.days, sample.days);
+    res.min.friendVouchers = Math.min(res.min.friendVouchers, sample.friendVouchers);
+    res.min.spareBodies = Math.min(res.min.spareBodies, sample.spareBodies);
+    res.min.advancedSpareBodies = Math.min(res.min.advancedSpareBodies, sample.advancedSpareBodies);
+    res.min.vouchers = Math.min(res.min.vouchers, sample.vouchers);
+    res.min.advancedVouchers = Math.min(res.min.advancedVouchers, sample.advancedVouchers);
+    res.min.highQualityVouchers = Math.min(res.min.highQualityVouchers, sample.highQualityVouchers);
+    res.min.middleQualityVouchers = Math.min(res.min.middleQualityVouchers, sample.middleQualityVouchers);
+  
+    // 最長
+    res.max.days = Math.max(res.max.days, sample.days);
+    res.max.friendVouchers = Math.max(res.max.friendVouchers, sample.friendVouchers);
+    res.max.spareBodies = Math.max(res.max.spareBodies, sample.spareBodies);
+    res.max.advancedSpareBodies = Math.max(res.max.advancedSpareBodies, sample.advancedSpareBodies);
+    res.max.vouchers = Math.max(res.max.vouchers, sample.vouchers);
+    res.max.advancedVouchers = Math.max(res.max.advancedVouchers, sample.advancedVouchers);
+    res.max.highQualityVouchers = Math.max(res.max.highQualityVouchers, sample.highQualityVouchers);
+    res.max.middleQualityVouchers = Math.max(res.max.middleQualityVouchers, sample.middleQualityVouchers);
+
+    // 平均
+    res.avg.days += sample.days / num;
+    res.avg.friendVouchers += sample.friendVouchers / num;
+    res.avg.spareBodies += sample.spareBodies / num;
+    res.avg.advancedSpareBodies += sample.advancedSpareBodies / num;
+    res.avg.vouchers += sample.vouchers / num;
+    res.avg.advancedVouchers += sample.advancedVouchers / num;
+    res.avg.highQualityVouchers += sample.highQualityVouchers / num;
+    res.avg.middleQualityVouchers += sample.middleQualityVouchers / num;
   }
   return res;
 }
@@ -274,7 +303,7 @@ const App = () => {
           </Box>
           <Box component="form" sx={{"& > :not(style)": {m: 1, width: "25ch"}}}>
             ジュエル
-            <TextField value={gems} onChange={e => setGems(e.target.value)} variant="outlined" size="small" style={{width: 120}} />
+            <TextField label="ジュエル" value={gems} onChange={e => setGems(e.target.value)} variant="outlined" size="small" style={{width: 120}} />
           </Box>
           <Box component="form" sx={{"& > :not(style)": {m: 1, width: "25ch"}}}>
             モールド
@@ -282,8 +311,8 @@ const App = () => {
             <TextField label="ハイクオリティ" value={highQualityMolds} onChange={e => setHighQualityMolds(e.target.value)} variant="outlined" size="small" style={{width: 150}} />
           </Box>
           <Box component="form" sx={{"& > :not(style)": {m: 1, width: "25ch"}}}>
-            フレンドポイント
-            <TextField value={friendPoints} onChange={e => setFriendPoints(e.target.value)} variant="outlined" size="small" style={{width: 150}} />
+            フレンド
+            <TextField label="ポイント" value={friendPoints} onChange={e => setFriendPoints(e.target.value)} variant="outlined" size="small" style={{width: 150}} />
           </Box>
           <Box component="form" sx={{"& > :not(style)": {m: 0.5, width: "2ch"}}}>
             ショップ
@@ -298,38 +327,54 @@ const App = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>#</TableCell>
+              <TableCell></TableCell>
               <TableCell>平均</TableCell>
+              <TableCell>最短</TableCell>
+              <TableCell>最長</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
               <TableCell>日数</TableCell>
-              <TableCell>{Math.floor(result.days)}日</TableCell>
+              <TableCell>{result.avg.days.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.days}</TableCell>
+              <TableCell>{result.max.days}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>一般募集</TableCell>
-              <TableCell>{Math.floor(result.vouchers)}回</TableCell>
+              <TableCell>{result.avg.vouchers.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.vouchers}</TableCell>
+              <TableCell>{result.max.vouchers}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>特別募集</TableCell>
-              <TableCell>{Math.floor(result.advancedVouchers)}回</TableCell>
+              <TableCell>{result.avg.advancedVouchers.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.advancedVouchers}</TableCell>
+              <TableCell>{result.max.advancedVouchers}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>ハイクオリティモールドガチャ</TableCell>
-              <TableCell>{Math.floor(result.highQualityVouchers)}回</TableCell>
+              <TableCell>{result.avg.highQualityVouchers.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.highQualityVouchers}</TableCell>
+              <TableCell>{result.max.highQualityVouchers}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>ミドルクオリティモールドガチャ</TableCell>
-              <TableCell>{Math.floor(result.middleQualityVouchers)}回</TableCell>
+              <TableCell>{result.avg.middleQualityVouchers.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.middleQualityVouchers}</TableCell>
+              <TableCell>{result.max.middleQualityVouchers}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>ソーシャルポイント募集</TableCell>
-              <TableCell>{Math.floor(result.friendVouchers)}回</TableCell>
+              <TableCell>{result.avg.friendVouchers.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.friendVouchers}</TableCell>
+              <TableCell>{result.max.friendVouchers}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>スペアボディ</TableCell>
-              <TableCell>{Math.floor(result.spareBodies)}体</TableCell>
+              <TableCell>{result.avg.spareBodies.toPrecision(3)}</TableCell>
+              <TableCell>{result.min.spareBodies}</TableCell>
+              <TableCell>{result.max.spareBodies}</TableCell>
             </TableRow>
           </TableBody>
         </Table>
